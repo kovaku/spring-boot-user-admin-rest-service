7450641f62fac823f6fad087c0755a67786ed1ee
Ferenc Szabo <v-fszabo@expediagroup.com>
2022-12-05 18:09:46 +0100

    Adding an example how to deserialise JSON response to a POJO

--- standalone-e2e-test/src/test/java/GetAllUsersTest.java
+++ standalone-e2e-test/src/test/java/GetAllUsersTest.java
@@ -1,12 +1,16 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+import response.UserResponse;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -70,5 +74,20 @@ public class GetAllUsersTest extends AbstractRestAssuredBase {
                 .asString();
 
         JSONAssert.assertEquals(expectedResponse, actualResponse, false);
+    }
+
+    @Test
+    //guide followed: https://www.toolsqa.com/rest-assured/deserialize-json-response/
+    void testDefaultGetAllUsersWithDeserializedJavaObject() {
+        Response actualResponse = RestAssured.given(getRequestSpecification())
+                .log().all()
+                .get();
+
+        ResponseBody actualBody = actualResponse.getBody();
+        List<UserResponse> userResponse = Arrays.asList(actualBody.as(UserResponse[].class));
+
+        Assertions.assertEquals(200, actualResponse.getStatusCode());
+        Assertions.assertEquals(1, userResponse.get(0).id);
+        Assertions.assertEquals("John Doe", userResponse.get(0).name);
     }
 }
